ac1979587c76aac7296763b0db01024787c8c92f
jodatm <joshuadtm@gmail.com>
2017-10-06 02:34:22 +0000

    4b completed

--- mpiEx4b.c
+++ mpiEx4b.c
@@ -27,27 +27,13 @@ int main(int argc,char *argv[]){
             double y = ((double)rand()) / ((double)RAND_MAX);
             if (x * x + y * y <= 1.0) circle ++;
         }
- 	printf("La suma de circulo es: %i\n",circle);
-	printf("Comenzo %i , termina %i\n",start,end);
-
-        //printf("%i :  %f\n",N[rank],(double)circle / N[rank] * 4.0);
-
-	/*int range = maxVal / size;
-	start = (range * rank)+1;
-	end = (range * (rank + 1));
-    	sum = 0;
-	printf("rank %i tiene start %i y end %i\n",rank,start,end);
-    	for(i=start; i<= end; i++){
-    		sum = sum +i;
-    	}*/
 
 	// Utilice la funcion 'MPI_Reduce' para guardar en la variable 
 	// 'sumTotal' la suma parcial de todos las tareas
 	MPI_Reduce(&circle, &circleTotal, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
 	if(rank==0){
-	  printf("El total de la suma es: %i\n",circleTotal);
-	  printf("%i :  %f\n",maxVal,(double)circle / maxVal * 4.0);
+	  printf("%i :  %f\n",maxVal,(double)circleTotal / maxVal * 4.0);
 	}
 
 	MPI_Finalize();
